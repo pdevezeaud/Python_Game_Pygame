5c11b524359cb0630c2b6d62ad0be101f5024926
Philippe Devezeau <pdevezeaud@yahoo.fr>
2022-06-20 18:49:42 +0200

    Ajout background deplacement

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from player import Player
 pygame.init()
 
+#Gestion du Frame/image
+clock = pygame.time.Clock()
+
 pygame.display.set_caption("Arcane Game")
 screen = pygame.display.set_mode((400,600))
 
@@ -11,13 +14,32 @@ background = pygame.image.load('ressource/ressource/fond.jpg').convert()
 #creation instance de player
 player = Player()
 
+#initialisation du background et de son positionnement en y
+y_background = 0
+
+
 #boucle d'animation
 running = True
 while running:
+    #Defilement fond ecran
 
-    screen.blit(background,(-109,0))
+    y_background += 1
+    if y_background < 619:
+        screen.blit(background,(-109,y_background))
+        screen.blit(background,(-109,y_background - 619))
+    else:
+        y_background = 0
+        screen.blit(background,(-109,y_background))
+        
     screen.blit(player.image,player.rect)
-    print(player.rect)
+    #print(player.rect)
+
+    #information sur le framerate
+    clock.tick(60)
+    print(f"{clock.get_fps()} FPS")
+
+
+
     pygame.display.flip()
 
     for event in pygame.event.get():
